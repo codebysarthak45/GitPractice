185488e5d24e4defc36c45e7d4109d1e25420ddf
Sartha Jadhav <sj02042003@gmail.com>
2025-02-23 14:18:25 +0530

    Second Program

---
+++ prg5BNT.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class prg5BNT
+{
+public static void main(String x[])
+  {
+    Scanner Sc=new Scanner(System.in);
+	System.out.printf("Enter the First string:");
+	String s=Sc.nextLine();
+	System.out.printf("Enter the Second String:");//check isomorphic
+	String s1=Sc.nextLine();
+	char ch[]=s.toCharArray();
+	char ch1[]=s1.toCharArray();
+	boolean flag=false;
+	for(int i=0;i<ch.length;i++)
+	{
+	   for(int j=i;i<ch.length;i++)
+	   {
+		   
+	   }
+	}
+  }
+}
